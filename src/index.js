e2ed6f6dac759cc4ee71f35e5a9b2dd68f74ac8b
darshjain <darshj786@gmail.com>
2021-06-24 00:26:53 +0530

    Throttled Searches and finalised project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM, { render } from 'react-dom'
 import YtSearch from 'youtube-api-search'
+import _ from 'lodash'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_details'
@@ -12,20 +13,26 @@ class App extends Component {
     super(props)
 
     this.state = { selectedVideo: null, videos: [] }
+    this.videoSearch('surfboards')
+  }
 
-    YtSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+  videoSearch(term) {
+    YtSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({ videos: videos, selectedVideo: videos[0] })
     })
   }
 
-
   render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term)
+    }, 300)
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
-          onVideoSelect={selectedVideo=> this.setState({selectedVideo})}
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
           videos={this.state.videos}
         />
       </div>
