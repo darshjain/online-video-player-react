66e9f415e611918f94dd698bca0b74d5f0031f2d
darshjain <darshj786@gmail.com>
2021-06-23 23:33:54 +0530

    Select Function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM, { render } from 'react-dom'
 import YtSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_details'
 
 const API_KEY = 'AIzaSyCEgOwFPp-Ij_6alAJ4u-NafQ1ktFQgn0s'
 
@@ -10,17 +11,23 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = { selectedVideo: null, videos: [] }
 
     YtSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos })
+      this.setState({ videos: videos, selectedVideo: videos[0] })
     })
   }
+
+
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo=> this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     )
   }
