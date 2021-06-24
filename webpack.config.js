0ac823949da88bf13cb75aa7e537a90a3f72ffef
Amey Bhavsar <28699912+ameybhavsar24@users.noreply.github.com>
2021-06-24 10:03:46 +0530

    fixed dotenv
    * added dotenv module
    * added dotenv plugin in webpack config
    * added TEST env variable
    * console.log TEST successfully in src/index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv').config({path: __dirname + '/.env'});
+const webpack = require('webpack');
 module.exports = {
   entry: ['./src/index.js'],
   output: {
@@ -26,5 +28,10 @@ module.exports = {
       aggregateTimeout: 300,
       poll: 1000
     }
-  }
+  },
+  plugins: [
+  	new webpack.DefinePlugin({
+  		"process.env": JSON.stringify(dotenv.parsed)
+  	}),
+  ]
 };
